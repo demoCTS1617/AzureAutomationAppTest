582ef40e6b000b19e19310c986b4c39a27f082be
demoCTS1617 <demo.nilesh@gmail.com>
2023-06-27 17:36:25 +0530

    Headless - third time commet

--- src/test/java/Apps/MyWebAppTest.java
+++ src/test/java/Apps/MyWebAppTest.java
@@ -3,13 +3,23 @@ package Apps;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 //import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 public class MyWebAppTest {
   @Test
   public void verifyTitle() throws InterruptedException {
-	  WebDriver driver = new ChromeDriver();
+	  
+
+	  ChromeOptions chrome_options = new ChromeOptions();
+	 
+	  chrome_options.addArguments("--headless");
+	   
+	  
+	  WebDriver driver = new ChromeDriver(chrome_options);
+	  
 	  driver.get("https://myazuredemo.azurewebsites.net/mywebProject/");
 	  
 	  String title = driver.getTitle();
